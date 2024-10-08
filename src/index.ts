3ab7327890f214b65cc8a8a80efacfc23b9f0463
tky0065 <tky0065@gmail.com>
2024-10-08 12:54:50 +0000

    starter

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/customers", async (req: Request, res: Response) => {
+  const customers = [
+    { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
+    {
+      name: "Joel Smith",
+      email: "joel.smith@example.com",
+      phone: "+0987654321",
+    },
+  ];
+
+  return res.status(200).json(customers);
+});
