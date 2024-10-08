1ea1da0d79b79665188ee1a0daf5f9d5d3c43034
tky0065 <tky0065@gmail.com>
2024-10-08 22:49:43 +0000

    (feat) updated customer ressources

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,29 +1,50 @@
 import { db } from "@/db/db";
-
 import { Request, RequestHandler, Response } from "express";
-
+import { StatusCodes } from "http-status-codes";
 
 export const createCustomer: RequestHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const { name, email, phone } = req.body;
+  const {
+    customerType,
+    firstName,
+    lastName,
+    phone,
+    gender,
+    maxCreditLimit,
+    maxCreditDays,
+    taxPin,
+    dob,
+    email,
+    nationalId,
+    country,
+    location,
+  } = req.body;
   try {
     const newCustomer = await db.customer.create({
       data: {
-        name,
-        email,
+        customerType,
+        firstName,
+        lastName,
         phone,
+        gender,
+        maxCreditLimit,
+        maxCreditDays,
+        taxPin,
+        dob,
+        email,
+        nationalId,
+        country,
+        location,
       },
-    })
+    });
 
     res.status(201).json(newCustomer);
-    
   } catch (error) {
     console.error(error);
-     res.status(500).json({ error: `Internal Server Error ${error}` });
+    res.status(500).json({ error: `Internal Server Error ${error}` });
   }
-
 };
 
 export const getCustomer: RequestHandler = async (
@@ -34,50 +55,105 @@ export const getCustomer: RequestHandler = async (
     const customers = await db.customer.findMany({
       orderBy: {
         createdAt: "desc",
-      }
+      },
     });
 
     res.status(200).json(customers);
   } catch (error) {
     console.error(error);
-     res.status(500).json({ error: `Internal Server Error ${error}` });
+    res.status(500).json({ error: `Internal Server Error ${error}` });
   }
-
-
 };
+
 export const getCustomerById: RequestHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   const { id } = req.params;
-  
-    try {
-      const customer = await db.customer.findUnique({
-        where: {
-          id: id,
-        }
-      })
-       res.status(200).json(customer);
-    } catch (error) {
-      console.error(error);
-       res.status(500).json({ error: `Internal Server Error ${error}` });
-    }
- 
+
+  try {
+    const customer = await db.customer.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json(customer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Internal Server Error ${error}` });
+  }
 };
+
 export const updateCustomer: RequestHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   const { id } = req.params;
-  const { name, email, phone } = req.body;
+  const {
+    customerType,
+    firstName,
+    lastName,
+    phone,
+    gender,
+    maxCreditLimit,
+    maxCreditDays,
+    taxPin,
+    dob,
+    email,
+    nationalId,
+    country,
+    location,
+  } = req.body;
 
   try {
     const updatedCustomer = await db.customer.update({
       where: { id: id },
-      data: { name, email, phone },
+      data: {
+        customerType,
+        firstName,
+        lastName,
+        phone,
+        gender,
+        maxCreditLimit,
+        maxCreditDays,
+        taxPin,
+        dob,
+        email,
+        nationalId,
+        country,
+        location,
+      },
     });
 
     res.status(200).json(updatedCustomer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: `Internal Server Error ${error}` });
+  }
+};
+
+export const deleteCustomer: RequestHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const existingCustomer = await db.customer.findUnique({
+      where: { id: id },
+    });
+    if (!existingCustomer) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: `Customer with id ${id} not found` });
+      return;
+    }
+    await db.customer.delete({
+      where: { id: id },
+    });
+
+    res
+      .status(StatusCodes.OK)
+      .json({ message: `Customer with id ${id} deleted`, error: null });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: `Internal Server Error ${error}` });
--- src/validators/customer.form.ts
+++ src/validators/customer.form.ts
@@ -1,15 +1,34 @@
 import { z } from 'zod';
 
 const createCustomerSchema = z.object({
-    name: z.string().min(1, "Name is required"),
+    customerType: z.enum(["RETAIL", "WHOLESALE", "DISTRIBUTOR", "OTHER"]),
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
     phone: z.string().min(1, "Phone is required"),
-    email: z.string().email("Invalid email address"),
+    gender: z.enum(["MALE", "FEMALE"]),
+    maxCreditLimit: z.number().min(0, "Max credit limit must be a positive number"),
+    maxCreditDays: z.number().min(0, "Max credit days must be a positive number"),
+    taxPin: z.string().optional(),
+    dob: z.string().optional(),
+    email: z.string().email("Invalid email address").optional(),
+    nationalId: z.string().optional(),
+    country: z.string().optional(),
+    location: z.string().min(1, "Location is required"),
 });
 
 const updateCustomerSchema = z.object({
     name: z.string().optional(),
     phone: z.string().optional(),
     email: z.string().email("Invalid email address").optional(),
+    location: z.string().optional(),
+    maxCreditLimit: z.number().optional(),
+    maxCreditDays: z.number().optional(),
+    taxPin: z.string().optional(),
+    dob: z.string().optional(),
+    nationalId: z.string().optional(),
+    country: z.string().optional(),
+    customerType: z.enum(["RETAIL", "WHOLESALE", "DISTRIBUTOR", "OTHER"]).optional(),
+    gender: z.enum(["MALE", "FEMALE"]).optional(),
 });
 
 export { createCustomerSchema, updateCustomerSchema };
