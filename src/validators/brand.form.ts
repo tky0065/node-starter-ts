fc59ad4ec3ecb71edb13a107e60137e77d43432f
tky0065 <tky0065@gmail.com>
2024-10-09 18:53:44 +0000

    fix(port): fiix port and stat commande

--- src/validators/brand.form.ts
+++ src/validators/brand.form.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 const createBrandSchema = z.object({
     id: z.string().optional(),
-    name: z.string().min(1, "Name must be at least 1 character long"),
-    slug: z.string().min(1, "Slug must be at least 1 character long"),
+    name: z.string().min(2, "Name must be at least 1 character long"),
+    slug: z.string().min(2, "Slug must be at least 1 character long"),
     products: z.array(z.any()).optional(),
     createdAt: z.date().optional(),
     updatedAt: z.date().optional(),
