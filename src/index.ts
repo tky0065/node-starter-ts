4bd9d97e071c472ec19fe4f947d38fcbed8c39e4
tky0065 <tky0065@gmail.com>
2024-10-09 18:51:34 +0000

    fix(port): fiix port and stat commande

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use("/api/v1", customerRouter);
